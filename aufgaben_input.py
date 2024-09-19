543c6bf6926c64337f746b639c2f0751341a99ca
Marco Kister <146673675+marcokister@users.noreply.github.com>
2024-09-19 08:29:24 +0200

    exercises Python print input variables

---
+++ aufgaben_input.py
@@ -0,0 +1,58 @@
+# Aufgabe 1
+#Begrüßung mit Namen
+#Ziel: Die Eingabe des Benutzers nutzen, um eine personalisierte Nachricht auszugeben.
+#Frage den Benutzer nach seinem Namen und gib eine personalisierte Begrüßung aus.
+#Die Antwort soll immmer als ausführlicher vollständiger Satz erfolgen!
+# -------------------- ab hier Deine Lösung ---------------------
+
+
+
+#Aufgabe 2
+#Einfacher Taschenrechner
+#Ziel: Einfache Berechnungen durchführen.
+#Nimm zwei Zahlen als Eingabe.
+#Führe eine Addition der beiden Zahlen durch und gib das Ergebnis aus.
+#Die Antwort soll immmer als ausführlicher vollständiger Satz erfolgen!
+# -------------------- ab hier Deine Lösung ---------------------
+
+
+
+#Aufgabe 3
+#Alter in 5 Jahren
+#Ziel: Mit Variablen arbeiten und das Alter in der Zukunft berechnen.
+#Frage den Benutzer nach seinem aktuellen Alter.
+#Berechne und gib aus, wie alt er in 5 Jahren sein wird.
+#Die Antwort soll immmer als ausführlicher vollständiger Satz erfolgen!
+# -------------------- ab hier Deine Lösung ---------------------
+
+
+
+#Aufgabe 4
+#Einfache Rechnung
+#Ziel: Multiplikation verwenden.
+#Frage den Benutzer nach einem Preis und einer Menge.
+#Berechne den Gesamtpreis und gib ihn aus.
+#Die Antwort soll immmer als ausführlicher vollständiger Satz erfolgen!
+# -------------------- ab hier Deine Lösung ---------------------
+
+
+
+#Aufgabe 5
+#Stunden in Tage umrechnen
+#Ziel: Variablen und einfache Mathematik verwenden.
+#Frage den Benutzer nach einer Anzahl von Stunden.
+#Berechne und gib aus, wie viele Tage das sind (1 Tag = 24 Stunden).
+#Die Antwort soll immmer als ausführlicher vollständiger Satz erfolgen!
+# -------------------- ab hier Deine Lösung ---------------------
+
+
+
+#Aufgabe 6
+#Rechteckfläche berechnen
+#Ziel: Einfache Berechnungen durchführen und die Ausgabe formatieren.
+#Frage den Benutzer nach der Länge und Breite eines Rechtecks.
+#Berechne die Fläche und gib das Ergebnis aus.
+#Die Antwort soll immmer als ausführlicher vollständiger Satz erfolgen!
+# -------------------- ab hier Deine Lösung ---------------------
+
+
